b250d043c172e19e8fe1e7452ac7e6a7d1e65c13
Marco Chin Fung Lam <marcolam1111@gmail.com>
2023-08-05 00:06:10 +0800

    Set up basic algorithm that takes user input for interval in seconds on home page, and a reminder message sent to the terminal

--- water.py
+++ water.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+scheduler = BackgroundScheduler()
 
 def hello():
     return "Hello World!"
@@ -17,9 +18,24 @@ def time_form():
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
     user_input = request.form['myInput']
-    return f'You entered {user_input}'
+    scheduler.add_job(func=remind_to_drink, trigger='interval', seconds=int(user_input))
+    scheduler.start()
+    return f'Reminder set for every {user_input} seconds'
 
-@app.route('/', methods=['POST', 'GET'])
+# def send_reminder():
+#     print("Take a sip of water!")
+
+# def set_reminder():
+#     scheduler = BackgroundScheduler()
+#     interval = request.form['myInput']
+#     scheduler.add_job(func=send_reminder, trigger='interval', seconds=interval)
+#     scheduler.start()
+
+#     return 'Reminder Set'
+
+@app.route('/')
+def home():
+    return render_template('time_form.html')
 
 
 
