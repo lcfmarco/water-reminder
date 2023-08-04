6790c499fe07c4983777f1824c07b63ea5b7b23b
Marco Chin Fung Lam <marcolam1111@gmail.com>
2023-08-05 00:07:25 +0800

    Clean up

--- water.py
+++ water.py
@@ -11,9 +11,9 @@ def remind_to_drink():
     # Prints reminder message t
     print("Take a sip of your water bottle!")
 
-@app.route('/time-form')
-def time_form():
-    return render_template('time_form.html')
+# @app.route('/time-form')
+# def time_form():
+#     return render_template('time_form.html')
 
 @app.route('/submit-form', methods=['POST'])
 def submit_form():
@@ -22,30 +22,11 @@ def submit_form():
     scheduler.start()
     return f'Reminder set for every {user_input} seconds'
 
-# def send_reminder():
-#     print("Take a sip of water!")
-
-# def set_reminder():
-#     scheduler = BackgroundScheduler()
-#     interval = request.form['myInput']
-#     scheduler.add_job(func=send_reminder, trigger='interval', seconds=interval)
-#     scheduler.start()
-
-#     return 'Reminder Set'
 
 @app.route('/')
 def home():
     return render_template('time_form.html')
 
-
-
-@app.route('/set-reminder/<int:interval>')
-
-def set_reminder(interval):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=remind_to_drink, trigger='interval', seconds=interval)
-    scheduler.start()
-    return f"Reminder set for every {interval} seconds"
 
 if __name__ == '__main__':
     app.run(debug=True)
